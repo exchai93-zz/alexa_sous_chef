33e7cf94b943cafe3ad8bac2308484805d193d48
emmpak <eiliev05@yahoo.com>
2017-05-23 10:34:59 +0100

    Add initialize and build methods to response class

--- lib/alexa/response.rb
+++ lib/alexa/response.rb
@@ -1,4 +1,15 @@
 module Alexa
-  class Response
+  class Response < Hash
+    def initialize
+      self[:version] = '1.0'
+      self[:response] = Hash.new
+      self[:response][:outputSpeech] = Hash.new
+      self[:response][:outputSpeech][:type] = "PlainText"
+      self[:response][:outputSpeech][:text] = "Hello World"
+    end
+
+    def self.build
+      new.to_json
+    end
   end
 end
