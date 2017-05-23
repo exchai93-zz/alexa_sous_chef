9ac96d462d2bd3a33892e0038e2c6ccb98c73b15
emmpak <eiliev05@yahoo.com>
2017-05-23 10:40:21 +0100

    Add custom reponse test

--- spec/alexa_response_spec.rb
+++ spec/alexa_response_spec.rb
@@ -2,7 +2,21 @@ require 'alexa/response'
 
 RSpec.describe Alexa::Response do
   describe '.build' do
-    it 'returns a minimal JSON response' do
+    it 'returns a JSON response with a custom string if provided' do
+      expected_response = {
+        version: "1.0",
+        response: {
+          outputSpeech: {
+            type: "PlainText",
+            text: "Custom String"
+          }
+        }
+      }.to_json
+
+      expect(Alexa::Response.build("Custom String")).to eq expected_response
+    end
+
+    it 'returns a minimal JSON response otherwise' do
       minimal_response = {
         version: "1.0",
         response: {
