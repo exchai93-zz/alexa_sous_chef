ac5457b504d0df7922b017f8debec0e31dd07186
emmpak <eiliev05@yahoo.com>
2017-05-23 10:30:09 +0100

    Add build test to response

--- spec/alexa_response_spec.rb
+++ spec/alexa_response_spec.rb
@@ -1,5 +1,19 @@
 require 'alexa/response'
 
 RSpec.describe Alexa::Response do
-  
+  describe '.build' do
+    it 'returns a minimal JSON response' do
+      minimal_response = {
+        version: "1.0",
+        response: {
+          outputSpeech: {
+            type: "PlainText",
+            text: "Hello World"
+          }
+        }
+      }.to_json
+
+      expect(Alexa::Response.build).to eq minimal_response
+    end
+  end
 end
