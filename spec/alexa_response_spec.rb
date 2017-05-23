6262fadf75ad5b01777d0278526260eb4b714f77
emmpak <eiliev05@yahoo.com>
2017-05-23 15:48:28 +0100

    Add session test for Response class

--- spec/alexa_response_spec.rb
+++ spec/alexa_response_spec.rb
@@ -2,6 +2,24 @@ require 'alexa/response'
 
 RSpec.describe Alexa::Response do
   describe '.build' do
+    it 'returns a JSON response with session data if prodivded' do
+      expected_response = {
+        version: "1.0",
+        sessionAttributes: {
+          sessionKey: "Session Value"
+        },
+        response: {
+          outputSpeech: {
+            type: "PlainText",
+            text: "Custom String"
+          }
+        }
+      }.to_json
+
+      session_response = Alexa::Response.build("Hello World", { sessionKey: "Session Value" })
+      expect(session_response).to eq expected_response
+    end
+
     it 'returns a JSON response with a custom string if provided' do
       expected_response = {
         version: "1.0",
