9ce33b05dac303fe118b34103a4621fefeb12925
emmpak <eiliev05@yahoo.com>
2017-05-23 16:01:09 +0100

    Add end_session argument to Response class

--- lib/alexa/response.rb
+++ lib/alexa/response.rb
@@ -1,16 +1,17 @@
 module Alexa
   class Response < Hash
-    def initialize(response_text, session_attributes)
+    def initialize(response_text, session_attributes, end_session)
       self[:version] = '1.0'
       self[:sessionAttributes] = session_attributes unless session_attributes.empty?
       self[:response] = Hash.new
       self[:response][:outputSpeech] = Hash.new
       self[:response][:outputSpeech][:type] = "PlainText"
       self[:response][:outputSpeech][:text] = response_text
+      self[:response][:shouldEndSession] = end_session if end_session
     end
 
-    def self.build(response_text = "Hello Chef", session_attributes = {})
-      new(response_text, session_attributes).to_json
+    def self.build(response_text = "Hello Chef", session_attributes = {}, end_session = false)
+      new(response_text, session_attributes, end_session).to_json
     end
   end
 end
