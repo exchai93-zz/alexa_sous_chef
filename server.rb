94dd53346d98c392e97d62103c892e358011ce9b
emmpak <eiliev05@yahoo.com>
2017-05-24 10:14:29 +0100

    Add recipe to session attributes

--- server.rb
+++ server.rb
@@ -5,6 +5,11 @@ require_relative './lib/alexa/response'
 
 post '/' do
   alexa_request = Alexa::Request.new(request)
-  response_text = "Found #{alexa_request.slot_value("Recipe")}"
-  Alexa::Response.build(response_text)
+
+  if alexa_request.intent_name == "FindRecipe"
+    recipe_name = alexa_request.slot_value("Recipe")
+    # recipe = File.read("sample_json.rb")
+    response_text = "Found" + recipe_name
+    return Alexa::Response.build(response_text: response_text, session_attributes: { recipeName: recipe_name })
+  end
 end
