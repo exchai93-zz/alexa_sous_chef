c2b83767420ce128733fd2718b5b295501f6b965
emmpak <eiliev05@yahoo.com>
2017-05-24 09:07:12 +0100

    Add session_attribute test in request

--- spec/alexa_request_spec.rb
+++ spec/alexa_request_spec.rb
@@ -23,4 +23,21 @@ RSpec.describe Alexa::Request do
       expect(Alexa::Request.new(sinatra_request).slot_value("SlotName")).to eq "10"
     end
   end
+
+  describe '#session_attribute' do
+    it 'is true if this a new session' do
+      request_json = {
+        "session": {
+          "sessionIDd": "id_string",
+          "attributes": {
+            "recipe": "Chocolate Cake"
+          }
+        }
+      }.to_json
+
+      sinatra_request = double("Sinatra::Request", body: StringIO.new(request_json))
+
+      expect(Alexa::Request.new(sinatra_request).session_attribute("recipe")).to eq "Chocolate Cake"
+    end
+  end
 end
