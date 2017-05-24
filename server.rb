df0b2cdf5534c3bc71ebf084f450c668d21337b6
emmpak <eiliev05@yahoo.com>
2017-05-24 12:14:43 +0100

    WIP: manipulating directions hash

--- server.rb
+++ server.rb
@@ -8,8 +8,35 @@ post '/' do
 
   if alexa_request.intent_name == "FindRecipe"
     recipe_name = alexa_request.slot_value("Recipe")
-    # recipe = File.read("sample_json.rb")
     response_text = "Found" + recipe_name
     return Alexa::Response.build(response_text: response_text, session_attributes: { recipeName: recipe_name })
   end
+
+  if alexa_request.intent_name == 'Steps'
+    recipe_name = alexa_request.session_attribute("recipeName")
+    action = alexa_request.slot_value("Action")
+    recipe = JSON.parse(File.read("sample_json.rb"))
+    stepNumber = 1 || alexa_request.session_attribute("stepNumber")
+
+    if alexa_request.slot_value("Action") == 'start cooking'
+      response_text = recipe['recipe']['directions']['direction']['direction_description']
+      stepNumber += 1
+    end
+
+    if alexa_request.slot_value("Action") == 'next'
+      step = recipe['recipe']['directions'].keys[stepNumber]
+      p recipe['recipe']['directions'].keys
+
+      # p stepNumber
+      # direction = recipe['recipe']['directions'].each.select { |direction| p direction; direction[1]['direction_number'].to_i == stepNumber }
+      # p direction
+      response_text = step['direction_description']
+      p response_text
+      stepNumber += 1
+    end
+
+    return Alexa::Response.build(response_text: response_text, session_attributes: { recipeName: recipe_name, stepNumber: stepNumber })
+  end
+
+
 end
