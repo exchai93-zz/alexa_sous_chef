d6a503c74fefe8aee055d79e436c1e59e5b038a4
Emily Chai <echai93@gmail.com>
2017-05-28 12:06:34 +0100

    WIP implement separate methods for reading ingredients

--- server.rb
+++ server.rb
@@ -19,6 +19,14 @@ class AlexaChef < Sinatra::Base
     end
 
     if alexa_request.intent_name == 'Ingredients'
+      return respond_with_options(alexa_request)
+    end
+
+    if alexa_request.intent_name == 'StepByStep'
+      return respond_with_ingredients_steps(alexa_request)
+    end
+
+    if alexa_request.intent_name == 'Altogether'
       return respond_with_ingredients(alexa_request)
     end
 
@@ -59,10 +67,18 @@ class AlexaChef < Sinatra::Base
       return Alexa::Response.build(response_text: response_text, session_attributes: { recipe: recipe.contents })
     end
 
+    def respond_with_options(alexa_request)
+      response_text = "How would you like the ingredients read? Step by step or altogether?"
+      return Alexa::Response.build(response_text: response_text)
+    end
+
     def respond_with_ingredients(alexa_request)
       recipe = Recipe.new(alexa_request.session_attribute('recipe'))
       response_text = "Here are the ingredients: " + recipe.ingredients
       return Alexa::Response.build(response_text: response_text, session_attributes: { recipe: recipe.contents })
+    end
+
+    def respond_with_ingredients_steps(alexa_request)
     end
 
     def respond_with_step(alexa_request)
