1ec0b3883053978aac3361d5cc3eec59c3c27ad0
emmpak <eiliev05@yahoo.com>
2017-05-29 18:08:42 +0100

    Stub API

--- intents/find_recipe.rb
+++ intents/find_recipe.rb
@@ -2,16 +2,16 @@ require './lib/recipe'
 
 intent "FindRecipe" do
   available_choices = [1,2,3,4,5]
+  user_input = request.slot_value('Recipe').to_i
 
   if available_choices.include? user_input
     # Stubbed API
-    # recipe = Recipe.new(JSON.parse(File.read("recipe_json.rb")))
+    recipe = Recipe.new(JSON.parse(File.read("recipe_json.rb")))
     # API
-    formatted_recipes = request.session_attribute('recipes')
-    user_input = request.slot_value('Recipe').to_i
-    
-    recipe_id = formatted_recipes[user_input - 1].values.pop.to_i
-    recipe = Recipe.find(recipe_id)
+    # formatted_recipes = request.session_attribute('recipes')
+    #
+    # recipe_id = formatted_recipes[user_input - 1].values.pop.to_i
+    # recipe = Recipe.find(recipe_id)
 
     response_text = "Found " + recipe.name
     respond(response_text: response_text, session_attributes: { recipe: recipe.contents })
