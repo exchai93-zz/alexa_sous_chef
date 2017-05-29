47f4402961ee561cb23b78a6dd27f19c8a59fd31
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-05-29 16:42:28 +0100

    adds choice validation

--- intents/find_recipe.rb
+++ intents/find_recipe.rb
@@ -1,6 +1,7 @@
 require './lib/recipe'
 
 intent "FindRecipe" do
+  available_choices = %w(1 2 3 4 5)
   # Stubbed API
   recipe = Recipe.new(JSON.parse(File.read("recipe_json.rb")))
   # API
@@ -8,6 +9,12 @@ intent "FindRecipe" do
   # choice = alexa_request.slot_value('Recipe').to_i - 1
   # recipe_id = formatted_recipes[choice].values.pop.to_i
   # recipe = Recipe.find(recipe_id)
-  response_text = "Found " + recipe.name
-  respond(response_text: response_text, session_attributes: { recipe: recipe.contents })
+
+  if available_choices.include? request.slot_value('Recipe')
+    response_text = "Found " + recipe.name
+    respond(response_text: response_text, session_attributes: { recipe: recipe.contents })
+  else
+    response_text = "Sorry, that is not a valid choice. Please pick from choices 1 to 5."
+    respond(response_text: response_text)
+  end
 end
