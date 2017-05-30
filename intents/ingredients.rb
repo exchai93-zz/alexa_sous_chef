68959b4c267e72c0cc7c06acd016d0b4fe5ebe14
emmpak <eiliev05@yahoo.com>
2017-05-30 08:10:44 +0100

    Include directions for the two approaches in ingredients intent

--- intents/ingredients.rb
+++ intents/ingredients.rb
@@ -4,7 +4,9 @@ intent "Ingredients" do
   recipe = Recipe.new(request.session_attribute('recipe'))
   method = request.slot_value('Ingredients')
 
-  if method =~ /step || one || next/
+  if method =~ /[iI]ngredient/
+    respond(response_text: "Would you like to hear the ingredients one at a time or altogether? If you choose the former, say next to hear the next ingredient.", session_attributes: { recipe: recipe.content })
+  elsif method =~ /one || next/
     ingredient_number = request.session_attribute('ingredient_number') || 0
     respond(response_text: recipe.ingredients[ingredient_number], session_attribute: { recipe: recipe.contents, ingredient_number: ingredient_number})
   else
