82168b279590fe85adff6e34aad2dea06e698248
emmpak <eiliev05@yahoo.com>
2017-05-30 08:17:27 +0100

    Add a response for last ingredient

--- intents/ingredients.rb
+++ intents/ingredients.rb
@@ -8,7 +8,9 @@ intent "Ingredients" do
     respond(response_text: "Would you like to hear the ingredients one at a time or altogether? If you choose the former, say next to hear the next ingredient.", session_attributes: { recipe: recipe.content })
   elsif method =~ /one || next/
     ingredient_number = request.session_attribute('ingredient_number') || 0
-    respond(response_text: recipe.ingredients[ingredient_number], session_attribute: { recipe: recipe.contents, ingredient_number: ingredient_number})
+    ingredient = recipe.ingredinets[ingredient_number]
+    ingredient_number == recipe.ingredients.count - 1 ? response_text = "The last ingredient is #{ingredient}" : response_text = ingredient
+    respond(response_text: ingredient, session_attribute: { recipe: recipe.contents, ingredient_number: ingredient_number})
   else
     response_text = "Here are the ingredients: " + recipe.ingredients.join(', ')
     respond(response_text: response_text, session_attributes: { recipe: recipe.contents })
