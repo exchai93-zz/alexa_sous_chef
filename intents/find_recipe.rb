c39b7920748083b6bc01e58c20b002ed3f73b026
emmpak <eiliev05@yahoo.com>
2017-05-31 11:37:42 +0100

    Make API calls and change steps intent name

--- intents/find_recipe.rb
+++ intents/find_recipe.rb
@@ -3,16 +3,15 @@ require './lib/recipe'
 intent "FindRecipe" do
   available_choices = [1,2,3,4,5]
   user_input = request.slot_value('Choice').to_i
-  p user_input
 
   if available_choices.include? user_input
     # Stubbed API
-    recipe = Recipe.new(JSON.parse(File.read("recipe_json.rb")))
+    # recipe = Recipe.new(JSON.parse(File.read("recipe_json.rb")))
     # API
-    # formatted_recipes = request.session_attribute('recipes')
-    #
-    # recipe_id = formatted_recipes[user_input - 1].values.pop.to_i
-    # recipe = Recipe.find(recipe_id)
+    formatted_recipes = request.session_attribute('recipes')
+
+    recipe_id = formatted_recipes[user_input - 1].values.pop.to_i
+    recipe = Recipe.find(recipe_id)
 
     response_text = "Found #{recipe.name}. To continue, ask for the ingredients or the preparation steps"
     respond(response_text: response_text, session_attributes: { recipe: recipe.contents })
